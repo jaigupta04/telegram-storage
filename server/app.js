2906bdc1f5bc642327d3e4cc53da8155a80ec815
Jai Gupta <jaiwork04@gmail.com>
2025-08-27 16:59:52 +0530

    vercel changes

--- server/app.js
+++ server/app.js
@@ -17,4 +17,6 @@ app.use('/', fileRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
